a6969664707aaedb71853edfd161164e13b109e6
Andrew Fillenwarth <andyf016@gmail.com>
2020-07-02 15:13:50 -0400

    passing short

--- anagrams.py
+++ anagrams.py
@@ -13,11 +13,33 @@ for an arbitrary list of strings.
 __author__ = "???"
 
 import sys
+import cProfile
+import io
+import pstats
+# from collections import defaultdict
 
+def profile(func):
+    """A cProfile decorator function that can be used to
+    measure performance.
+    """
+    # This profiling decorator was adapted from the Python 3.6 Docs
+    def inner(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = func(*args, **kwargs)
+        pr.disable()
+        string = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=string).sort_stats(sortby)
+        ps.print_stats()
+        print(string.getvalue())
+        return retval
+
+    return inner
 
 def alphabetize(string):
     """Returns alphabetized version of the string."""
-    return "".join(sorted(string.lower()))
+    return "".join(sorted(string))
 
 
 def find_anagrams(words):
@@ -27,21 +49,31 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
-    return anagrams
+ #   anagrams = {
+ #       alphabetize(word): [
+ #           w for w in words
+ #           if alphabetize(w) == alphabetize(word)]
+ #       for word in words}
+    
+    anagrams2 = {}
+    for word in words:
+        anagrams2.setdefault("".join(sorted(word)), [])
+    for word in words:
+        vari = "".join(sorted(word))
+        if vari in anagrams2:
+            anagrams2[vari].append(word)
+    return anagrams2
 
+the_file = 'words/short.txt'
 
+@profile
 def main(args):
     # run find_anagrams() on first argument filename
-    if len(args) < 1:
-        print("Please specify a word file!")
-        sys.exit(1)
+    #if len(args) < 1:
+    #    print("Please specify a word file!")
+    #    sys.exit(1)
 
-    with open(args[0]) as f:
+    with open(the_file) as f:
         words = f.read().split()
     anagram_dict = find_anagrams(words)
     for k, v in anagram_dict.items():
@@ -49,4 +81,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
