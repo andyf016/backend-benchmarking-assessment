16288e631d055cae54658e9cbf3a4b4048ca9d15
Andrew Fillenwarth <andyf016@gmail.com>
2020-07-02 23:30:10 -0400

    Passing all tests, cleaned up code

--- anagrams.py
+++ anagrams.py
@@ -10,18 +10,19 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Andrew Fillenwarth"
 
 import sys
 import cProfile
 import io
 import pstats
-# from collections import defaultdict
+
 
 def profile(func):
     """A cProfile decorator function that can be used to
     measure performance.
     """
+    # Profiler to profile main()
     # This profiling decorator was adapted from the Python 3.6 Docs
     def inner(*args, **kwargs):
         pr = cProfile.Profile()
@@ -37,10 +38,6 @@ def profile(func):
 
     return inner
 
-def alphabetize(string):
-    """Returns alphabetized version of the string."""
-    return "".join(sorted(string))
-
 
 def find_anagrams(words):
     """
@@ -49,12 +46,6 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
- #   anagrams = {
- #       alphabetize(word): [
- #           w for w in words
- #           if alphabetize(w) == alphabetize(word)]
- #       for word in words}
-    
     anagrams2 = {}
     for word in words:
         anagrams2.setdefault("".join(sorted(word)), [])
@@ -64,16 +55,15 @@ def find_anagrams(words):
             anagrams2[vari].append(word)
     return anagrams2
 
-the_file = 'words/short.txt'
 
-@profile
+# @profile
 def main(args):
     # run find_anagrams() on first argument filename
-    #if len(args) < 1:
-    #    print("Please specify a word file!")
-    #    sys.exit(1)
+    if len(args) < 1:
+        print("Please specify a word file!")
+        sys.exit(1)
 
-    with open(the_file) as f:
+    with open(args[0]) as f:
         words = f.read().split()
     anagram_dict = find_anagrams(words)
     for k, v in anagram_dict.items():
@@ -81,4 +71,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
